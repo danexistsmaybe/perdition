1cc023a3d4f8eef6688e903c70b799fc79769d77
Daniel Evans <danevans264@gmail.com>
2025-06-14 22:29:06 -0500

    Tiles

---
+++ Scripts/Debug/follow_mouse.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+var speed := 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _physics_process(delta):
+	position += speed*(get_viewport().get_mouse_position() - position).normalized()
